f54778ce05f93295c818adb5d1ef3d9a0c91ec10
Sarthak Tarsariya <sarthakrajubhai@gmail.com>
2025-10-04 16:27:35 +0000

    Object in value Access ,Insert ,delete ,Update

---
+++ Q4.js
@@ -0,0 +1,23 @@
+// Q4: object in value add,remove and how to object in value access
+
+//creta object
+const obj1={
+    name:"sarthak",
+    isLogedIn:true,
+    city:"surat"
+}
+console.log(obj1)
+
+//object value access to dot Notation
+
+console.log(obj1.name)
+
+// object in add value 
+
+obj1["Language"]="JavaScript"
+console.log(obj1)
+
+//object innvalue delete use delete keyword
+
+ delete obj1["Language"]
+ console.log(obj1) 
