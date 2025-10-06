852f34b425cc4680274b0fc1b53a0b1fc4bcc0c9
Sarthak Tarsariya <sarthakrajubhai@gmail.com>
2025-10-06 16:58:09 +0000

    Array Ascending order and descending order

--- Q12.js
+++ Q12.js
@@ -27,3 +27,20 @@ function arrayAscending(arr) {
 }
 
 console.log(arrayAscending([5,4,7,2,1,6,]))
+
+function sortArrayDesc(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - 1 - i; j++) {
+            if (arr[j] < arr[j + 1]) {
+                // Swap the elements
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(sortArrayDesc([5, 3, 8, 1]));
