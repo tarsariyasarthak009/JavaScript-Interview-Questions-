f5a97430864b8deb26dfb6c70b57fddef766740a
Sarthak Tarsariya <sarthakrajubhai@gmail.com>
2025-10-06 16:52:25 +0000

    CountChar

--- Q11.js
+++ Q11.js
@@ -4,3 +4,16 @@ function countChar(str,char) {
    return str.split(char).length-1
 }
 console.log(countChar('sarthaktarsariya', 'a'));
+
+
+function countChar1(str,char)//this function in use loop and check the if condtion 
+{
+   let count=0;
+   for (let i = 1; i < str.length; i++) {
+      if (str[i]==char) {
+         count++
+      }
+   }
+   return count
+}
+console.log(countChar1("sarthaktasariya","a"))
